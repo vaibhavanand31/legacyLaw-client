5f5420f95613092f6880291ec087cbfff2b94824
vaibhavanand31 <vaibhavanand1591>
2020-03-24 18:00:24 +0530

    update24march2020

--- client/src/Components/Common/index.js
+++
@@ -1,5 +0,0 @@
-import Header from './Header/Header';
-import Footer from './Footer';
-import TopSidenav from './TopSidenav';
-
-export { Header, Footer, TopSidenav };
--- client/src/Components/NotFound/NotFound.js
+++ client/src/Components/NotFound/NotFound.js
@@ -9,14 +9,14 @@ function NotFound() {
 		<div className={styles.pageNotFound}>
 			<div className={styles.notfound}>
 				<div className={styles.notfound404}>
-					<h3>Oops! Page not found</h3>
+					<h3>Oops! You Broke it</h3>
 					<h1>
 						<span>4</span>
 						<span>0</span>
 						<span>4</span>
 					</h1>
 				</div>
-				<h2>we are sorry, but the page you requested was not found</h2>
+				<h2>Or you'r just being silly.</h2>
 				<h2>
 					<Button
 						variant="outlined"
